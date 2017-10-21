993e8e7c78ccda2349388f7d3d83472b912981a5
thomas.caycedo <thomascaycedo19@outlook.com>
2017-10-21 02:03:15 -0500

    Se añade una accion presionando .

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-var i=0;
-var txt = "pedro por favor responde esta pregunta tan importante";
-var t="";
-function initLogic(e){
-    console.log(e.key);
-    e.preventDefault();
-    e.stopPropagation();
-    t= t+txt.charAt(i);
-    $(e.target).val(""+t+"");
-    i++;
-}
+var i = 0;
+var txt = "Pedro por favor responde esta pregunta tan importante";
+var t = "";
+var respu = [];
+
+function initLogic(e) {
+
+	console.log(e.key);
+	e.preventDefault();
+	e.stopPropagation();
+	t = t + txt.charAt(i);
+	$(e.target).val("" + t + "");
+	i++;
+
+	if (e.keyCode == 190) {
+
+			respuesta.innerHTML = txt;
+		}
+		//respuesta.innerHTML = respu.length;
+	}
+
+
