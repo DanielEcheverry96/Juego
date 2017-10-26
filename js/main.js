3dc3797cd662791c9037c0d5b140a4b2636d59b5
thomas.caycedo <thomascaycedo19@outlook.com>
2017-10-26 17:56:42 -0500

    Se borran variables que no se necesitan

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
-var contador = 0;
-var len = 0;
-var bandera = 0;
-var letra = "";
-var temp = "";
-var ans = "";
 var rem = "";
 var t="";
 var i = 0;
-var n = 1;
 
 var peticion = "Pedro por favor responde esta pregunta tan importante, no me dejes en visto"
 
 function initLogic(e) {
 
-    console.log(e.key);
+    //console.log(e.key);
     e.preventDefault();
     e.stopPropagation();
     rem = rem + e.key;
