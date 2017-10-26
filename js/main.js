2eac9278787adcdf3819e31c06faafb78776a6c2
FelipePineda <henaopipepineda@gmail.com>
2017-10-26 03:05:20 -0500

    cambio en el js agregando variables y un contador

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var contador = 0;
 var len = 0;
 var bandera = 0;
 var letra = "";
-var respuesta = "";
 var temp = "";
 var ans = "";
 var rem = "";
@@ -10,39 +9,44 @@ var n = 1;
 
 function initLogic(e) {
 
-	console.log(e.key);
-	e.preventDefault();
-	e.stopPropagation();
-	temp = event.key
+    console.log(e.key);
+    e.preventDefault();
+    e.stopPropagation();
+    temp = event.key
 
-var frase = ["P", "e", "d", "r", "o", " ", "r","e", "s", "p","o","n","d","e"," ","e","s","t","a"," ","p","r","e","g","u","n","t","a"," ","p","o","r"," ","f","a","v","o","r"];
+    var frase = ["P", "e", "d", "r", "o", " ", "r", "e", "s", "p", "o", "n", "d", "e", " ", "e", "s", "t", "a", " ", "p", "r", "e", "g", "u", "n", "t", "a", " ", "p", "o", "r", " ", "f", "a", "v", "o", "r"];
+
+    var t = "";
+    var i = 0;
+    t = t + frase.charAt(i);
+    $(e.target).val("" + t + "");
+    i++;
 
 
+    while (n = 1) {
 
-while(n=1){
-	
 
-	temp=e.key;
-	if (temp == '.') {
-		bandera = 1;
-		respuesta.innerHTML=frase[contador++];
-		while(1){
-			temp=e.key;
-			respuesta.innerHTML=(frase[contador++]);
-			if(temp=='.')break;
-			ans[len++]=temp;
-		}
-		get (rem);
-		break;
-	}
-	break;	
+        temp = e.key;
+        if (temp == '.') {
+            bandera = 1;
+            respuesta.innerHTML = frase[contador++];
+            while (1) {
+                temp = e.key;
+                respuesta.innerHTML = (frase[contador++]);
+                if (temp == '.') break;
+                ans[len++] = temp;
+            }
+            get(rem);
+            break;
+        }
+        break;
+
+    }
 
 }
 
-}
 function preguntar() {
 
-	respuesta.innerHTML = ans;
+    respuesta.innerHTML = ans;
 
 }
-
