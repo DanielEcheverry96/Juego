5f121687fba6b96d412f3d3f328dcfb2f2c12b45
thomas.caycedo <thomascaycedo19@outlook.com>
2017-10-26 02:05:22 -0500

    Se modifica el js con while para dar funcionaliad

--- js/main.js
+++ js/main.js
@@ -1,31 +1,48 @@
 var contador = 0;
-var bandera = false;
+var len = 0;
+var bandera = 0;
 var letra = "";
 var respuesta = "";
-
+var temp = "";
+var ans = "";
+var rem = "";
+var n = 1;
 
 function initLogic(e) {
 
 	console.log(e.key);
 	e.preventDefault();
 	e.stopPropagation();
-	captura = event.key
+	temp = event.key
 
 var frase = ["P", "e", "d", "r", "o", " ", "r","e", "s", "p","o","n","d","e"," ","e","s","t","a"," ","p","r","e","g","u","n","t","a"," ","p","o","r"," ","f","a","v","o","r"];
 
-while(1){
-	
-	if (e.keyCode == 190) {
 
-	}
+
+while(n=1){
 	
+
+	temp=e.key;
+	if (temp == '.') {
+		bandera = 1;
+		respuesta.innerHTML=frase[contador++];
+		while(1){
+			temp=e.key;
+			respuesta.innerHTML=(frase[contador++]);
+			if(temp=='.')break;
+			ans[len++]=temp;
+		}
+		get (rem);
+		break;
+	}
+	break;	
 
 }
 
 }
 function preguntar() {
 
-	respuesta.innerHTML = respu.substr(1,2);
+	respuesta.innerHTML = ans;
 
 }
 
