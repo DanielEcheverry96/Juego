f42b0224c96d7948931ba920e4eb2f29a92bb62c
FelipePineda <henaopipepineda@gmail.com>
2017-10-26 19:19:34 -0500

    Se añade funcion

--- js/main.js
+++ js/main.js
@@ -15,24 +15,31 @@ function initLogic(e) {
     i++; //avanza por la peticion
 }
 
-function preguntar() {
-    var re = ""; //va a almacenar el texto de punto a punto
-    var pos1 = -1; //se almacena el primer punto
-    var pos2 = -1; //se almacena el segundo punto
-    bandera = 0;
-    for (var w = 0; w < rem.length; w++) { //recorre el texto oculto busca el primer punto
+
+/**
+*
+*/
+function encontrarPunto(initial_pos){
+    var res = -1;
+    for (var w = initial_pos; w < rem.length; w++) { //recorre el texto oculto busca el primer punto
         if (rem.charAt(w) == '.') { //compara el if con cada posicion
-            pos1 = w; //iguala pos1 a el primer punto
+            res = w; //iguala pos1 a el primer punto
             break; // sale del for
         }
     }
+    return res;
+}
 
-    for (var x = pos1 + 1; x < rem.length; x++) { //empieza en la siguiente posicion despues del primer punto
-        if (rem.charAt(x) == '.') { //busca el segundo punto
-            pos2 = x; //iguala pos2 a el segundo punto
-            break; //sale del for
-        }
-    }
+/**
+* Esta fincion .....  
+* @function pregunta
+* @param {String} nom askdfjlaksjdfljañsldfj
+*/
+function preguntar() {
+    var re = ""; //va a almacenar el texto de punto a punto
+    var pos1 = encontrarPunto(0); //se almacena el primer punto
+    var pos2 = encontrarPunto(pos1+1); //se almacena el segundo punto
+    bandera = 0;
 
     for (var q = pos1 + 1; q < pos2; q++) {
         re = re + rem.charAt(q); //junta lo que hay dentro de pos1 y pos2 
@@ -46,6 +53,5 @@ function preguntar() {
     } else {
         respuesta.innerHTML = "Ups! No entendi, repite la pregunta";
     }
-
 
 }
